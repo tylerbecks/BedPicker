afdae7605f0e326f5c1b848d407e2189cbad2e73
Tyler Becks <tylerdbecks@gmail.com>
2018-12-28 04:07:47 -0800

    Add AssignmentScreen to tab nav

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -4,8 +4,7 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
+import AssignmentScreen from '../screens/AssignmentScreen';
 
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
@@ -25,36 +24,21 @@ HomeStack.navigationOptions = {
   ),
 };
 
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
+const BedStack = createStackNavigator({
+  Bed: AssignmentScreen,
 });
 
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
+BedStack.navigationOptions = {
+  tabBarLabel: 'Beds',
   tabBarIcon: ({ focused }) => (
     <TabBarIcon
       focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-link' : 'md-link'}
-    />
-  ),
-};
-
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? 'ios-options' : 'md-options'}
+      name={Platform.OS === 'ios' ? 'ios-bed' : 'md-bed'}
     />
   ),
 };
 
 export default createBottomTabNavigator({
   HomeStack,
-  LinksStack,
-  SettingsStack,
+  BedStack,
 });
