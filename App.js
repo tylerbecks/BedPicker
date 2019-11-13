01ef160c7f7f533ccc13869c8fe09a127b105735
Tyler Becks <tylerdbecks@gmail.com>
2019-11-12 19:19:22 -0800

    Refactor App to use hooks

--- App.js
+++ App.js
@@ -1,72 +1,69 @@
-import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { AppLoading } from 'expo';
-import * as Icon from '@expo/vector-icons';
-import * as Font from 'expo-font';
-import { Asset } from 'expo-asset';
-import AppNavigator from './navigation/AppNavigator';
+/* eslint-disable global-require */
+import React, { useState } from "react";
+import { Platform, StatusBar, StyleSheet, View } from "react-native";
+import { AppLoading } from "expo";
+import * as Icon from "@expo/vector-icons";
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
+import AppNavigator from "./navigation/AppNavigator";
 
-export default class App extends React.Component {
-  state = {
-    isLoadingComplete: false
-  };
-
-  render() {
-    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
-      return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
-        />
-      );
-    } else {
-      return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
-      );
-    }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
   }
+});
 
-  _loadResourcesAsync = async () => {
+export default ({ skipLoadingScreen }) => {
+  const [isLoadingComplete, setIsLoadingComplete] = useState(false);
+
+  const loadResourcesAsync = async () => {
     return Promise.all([
       Asset.loadAsync([
-        require('./assets/images/aaron.jpg'),
-        require('./assets/images/barak.jpg'),
-        require('./assets/images/cheryl.jpg'),
-        require('./assets/images/daniel.jpg'),
-        require('./assets/images/jeremy.jpg'),
-        require('./assets/images/josh.jpg'),
-        require('./assets/images/noah.jpg'),
-        require('./assets/images/raj.jpg'),
-        require('./assets/images/tyler.jpg')
+        require("./assets/images/aaron.jpg"),
+        require("./assets/images/barak.jpg"),
+        require("./assets/images/cheryl.jpg"),
+        require("./assets/images/daniel.jpg"),
+        require("./assets/images/jeremy.jpg"),
+        require("./assets/images/josh.jpg"),
+        require("./assets/images/noah.jpg"),
+        require("./assets/images/raj.jpg"),
+        require("./assets/images/tyler.jpg")
       ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
-        'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')
+        "space-mono": require("./assets/fonts/SpaceMono-Regular.ttf")
       })
     ]);
   };
 
-  _handleLoadingError = error => {
+  const handleLoadingError = error => {
     // In this case, you might want to report the error to your error
     // reporting service, for example Sentry
     console.warn(error);
   };
 
-  _handleFinishLoading = () => {
-    this.setState({ isLoadingComplete: true });
+  const handleFinishLoading = () => {
+    setIsLoadingComplete(true);
   };
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff'
+  if (!isLoadingComplete && !skipLoadingScreen) {
+    return (
+      <AppLoading
+        startAsync={loadResourcesAsync}
+        onError={handleLoadingError}
+        onFinish={handleFinishLoading}
+      />
+    );
   }
-});
+
+  return (
+    <View style={styles.container}>
+      {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+      <AppNavigator />
+    </View>
+  );
+};
